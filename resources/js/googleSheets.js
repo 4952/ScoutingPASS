05cdf7323664814592f70567353776498e81e2d3
Philippe Boulanger <philboul28@gmail.com>
2023-03-29 23:09:48 -0400

    Modifications for our team (4952)
    - Google sheets api
    - Blue alliance API

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -1,26 +1,28 @@
 function setUpGoogleSheets() {
-    const scriptURL = '<SCRIPT URL>'
-    const form = document.querySelector('#scoutingForm')
-    const btn = document.querySelector('#submit')
- 
-    
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      btn.disabled = true
-      btn.innerHTML = "Sending..."
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxVQuS47ClrV2-XCHK_6_avHboALiEpXzti7G8fWz5OvM4g5BA6Xizl2KqzK9NMBiM4/exec'
+  const form = document.querySelector('#scoutingForm')
+  const btn = document.querySelector('#submit')
 
-      let fd = getData(false)
-      for (const [key, value] of fd) {
-        console.log(`${key}: ${value}\n`);
-      }
 
-      fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: fd })
-        .then(response => { 
-              alert('Success!', response) })
-        .catch(error => {
-              alert('Error!', error.message)})
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    btn.disabled = true
+    btn.innerHTML = "Sending..."
 
-      btn.disabled = false
-      btn.innerHTML = "Send to Google Sheets"
-    })
+    let fd = getData(false)
+    for (const [key, value] of fd) {
+      console.log(`${key}: ${value}\n`);
+    }
+
+    fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: fd })
+      .then(response => {
+        alert('Success!', response)
+      })
+      .catch(error => {
+        alert('Error!', error.message)
+      })
+
+    btn.disabled = false
+    btn.innerHTML = "Send to Google Sheets"
+  })
 }
